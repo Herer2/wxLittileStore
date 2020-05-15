372a95751a2ed9b35ab1c2dd06db97e6fa5ab53a
Herer <763745033@qq.com>
2020-05-15 12:17:09 +0800

    second commit

---
+++ request/index.js
@@ -0,0 +1,36 @@
+// 同时发送异步请求的次数
+let ajaxTimes = 0;
+export const request = (params) => {
+
+    // 判断url中是否带有 /my/ 请求的是私有的路径 带上header token
+    let header = {...params.header };
+    if (params.url.includes('/my/')) {
+        // 拼接header 带上token
+        header['Authorization'] = wx.getStorageSync('token');
+    }
+
+    ajaxTimes++;
+    // 加载图标
+    wx.showLoading({
+        title: '加载中',
+        mask: true
+    });
+    return new Promise((resolve, reject) => {
+        wx.request({
+            ...params,
+            header: header,
+            success: (result) => {
+                resolve(result.data.message);
+            },
+            fail: (err) => {
+                reject(err);
+            },
+            complete: () => {
+                ajaxTimes--;
+                if (ajaxTimes === 0) {
+                    wx.hideLoading();
+                }
+            }
+        })
+    })
+}
